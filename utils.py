12cd85a2fc792ffcdfed941474ea29076c1307da
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2024-01-10 17:08:31 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ def find_match(input):
 def query_refiner(conversation, query):
     response = openai.completions.create(
         model="gpt-3.5-turbo-instruct",
-        prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+        prompt=f"Given the following user query, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base. \n\nQuery: {query}\n\nRefined Query:",
         temperature=0.7,
         max_tokens=256,
         top_p=1,
