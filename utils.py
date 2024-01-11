d982f6051292206699ed40e6f3f353f5a1ede6d0
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2024-01-12 02:22:32 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -33,9 +33,17 @@ def query_refiner(conversation, query):
 
 pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16, device_map="auto")
 
+messages = [
+    {
+        "role": "system",
+        "content": "You are a friendly chatbot who always responds in the style of a pirate",
+    },
+    {"role": "user", "content": "How many helicopters can a human eat in one sitting?"},
+]
+
 def query_refiner(conversation, query):
-    response = pipe(prompt = "Given the following user query, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base", 
-                     max_new_tokens=256, do_sample=True, temperature=0.7, top_k=5, top_p=0.95)
+    prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    response = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=5, top_p=0.95)
     
     return response.choices[0].text
 
