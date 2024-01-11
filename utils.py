3c57f4ae7b873c30ecf6466dfc10a180f3e6a2d8
Anushk Farkiya <45423129+Anushk97@users.noreply.github.com>
2024-01-12 01:22:25 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ openai.api_key = "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"
 model = SentenceTransformer('multi-qa-distilbert-cos-v1')
 
 pinecone.init(api_key='09d08617-45d2-4ce8-b708-d8291d5570d6', environment='gcp-starter')
-index = pinecone.Index('langchain-chatbot')
+index = pinecone.Index('langchain-chatbot-v2')
 
 
 def find_match(input):
